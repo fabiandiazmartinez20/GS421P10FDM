c70b19e04de95ef9f279b4f868545600073833d9
tesoem <tesoem@10.15.70.175>
2023-05-12 20:20:55 -0500

    trabajo del 12 de mayo

---
+++ src/Almacen/Archivos.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Almacen;
+
+import Informacion.Convertir;
+import Informacion.Datos;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tesoem
+ */
+public class Archivos {
+    List<Datos> contenido = new ArrayList<>();
+    
+    public List <Datos> Leer(){
+        String linea;
+        try {
+            FileReader archivo = new FileReader("Datos.txt");
+            BufferedReader br = new BufferedReader(archivo);
+            
+            while ((linea = br.readLine()) != null){
+                System.out.println("valor de linea= " + linea);
+  
+              
+           }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+        
+    }
+    public boolean Grabar(Datos cadena){
+        Convertir convertir = new Convertir();
+        boolean estado = true;
+        try {
+            FileWriter archivo = new FileWriter("datos.txt",true);
+            BufferedWriter bw = new BufferedWriter(archivo);
+            bw.write(convertir.ajson(cadena)+"\n");
+            bw.close();
+            archivo.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            estado = false;    
+        } 
+        return estado;
+        
+        
+    }
+}
